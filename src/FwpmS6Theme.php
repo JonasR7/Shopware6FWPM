fac1ba6c88dff4205e0827921e77cb80fd7740fd
JonasR7 <jonas-racky@t-online.de>
2023-01-19 16:55:27 +0100

    initial commit

---
+++ src/FwpmS6Theme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace FwpmS6Theme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class FwpmS6Theme extends Plugin implements ThemeInterface
+{
+}
