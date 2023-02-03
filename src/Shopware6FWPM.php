17ca47bf4b4c9449081628427a8b392cdfc41b1a
JonasR7 <jonas-racky@t-online.de>
2023-02-03 12:50:46 +0100

    initial commit

--- src/Shopware6FWPM.php
+++ src/Shopware6FWPM.php
@@ -1,10 +1,10 @@
 <?php declare(strict_types=1);
 
-namespace FwpmS6Theme;
+namespace Shopware6FWPM;
 
 use Shopware\Core\Framework\Plugin;
 use Shopware\Storefront\Framework\ThemeInterface;
 
-class FwpmS6Theme extends Plugin implements ThemeInterface
+class Shopware6FWPM extends Plugin implements ThemeInterface
 {
 }
